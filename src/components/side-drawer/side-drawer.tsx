006ce1c43c541f1bd33fe21b47898c3941e97470
Sarah Wimark <sarahwimark@gmail.com>
2019-01-02 20:35:12 +0100

    Adding style and functionality to open and close drawer

--- src/components/side-drawer/side-drawer.tsx
+++ src/components/side-drawer/side-drawer.tsx
@@ -14,13 +14,42 @@ import { Component, Prop } from '@stencil/core';
 export class SideDrawer {
     // {reflectToAttr: true} makes the attribute change reflect in the HTML
     @Prop({reflectToAttr: true}) title: string
-    @Prop() open: boolean
+    @Prop({reflectToAttr: true, mutable: true}) open: boolean
+
+    closeDrawer() {
+        this.open = false
+    }
+
+    changeContent(content: string) {
+
+    }
+    
     render() {
+        let mainContent = <slot></slot>
+        mainContent = (
+            <div id="contact-info">
+                <h2>Contact Information</h2>
+                <p>You can reach us via phone or email.</p>
+                <ul>
+                    <li>Phone: 9884734987487294</li>
+                    <li>E-Mail:{' '} 
+                    <a href="mailto:something@something.com">something@something.com</a>
+                    </li>
+                </ul>
+            </div>
+        )
         return (
             <aside>
-                <header><h1>{this.title}</h1></header>
+                <header>
+                    <h1>{this.title}</h1>
+                    <button onClick={this.closeDrawer.bind(this)}>X</button>
+                </header>
+                <section id="tabs"> 
+                    <button class="active" onClick={this.changeContent.bind(this, 'nav')}>Navigation</button>
+                    <button onClick={this.changeContent.bind(this, 'contact')}>Contact</button>
+                </section>
                 <main>
-                    <slot></slot>
+                    {mainContent}
                 </main>
             </aside>
         )
